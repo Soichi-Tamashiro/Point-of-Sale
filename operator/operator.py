c00e39e913c92d40b25ed7feb3843b21fb7b3ff6
Soichi Tamashiro <wyvernys@live.com>
2020-07-22 22:37:42 -0500

    On branch master  Your branch is up to date with 'origin/master'.
     Changes to be committed:
    	new file:   operator/operator.kv
    	new file:   operator/operator.py

---
+++ operator/operator.py
@@ -0,0 +1,20 @@
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+
+
+class OperatorWindow(BoxLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def update_purchases(self):
+        code = self.ids.code_inp.text
+
+
+class OperatorApp(App):
+    def build(self):
+        return OperatorWindow()
+
+
+if __name__ == "__main__":
+    oa = OperatorApp()
+    oa.run()
